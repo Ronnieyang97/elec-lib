0946de9aff8675fe819ed5a9b02c42a2a93a0fe1
Ronnie <1017268540@qq.com>
2019-05-29 13:37:22 +0800

    update fuzzy search

--- eleclib.py
+++ eleclib.py
@@ -19,10 +19,8 @@ class Lib(object):
         if num == '1':   #功能1按照书名查找
             self.findbytitle()
         elif num == '2':    #功能2按照作者查找
-            print("author")
             self.findbyauthor()
         elif num == '3':        #功能3按照类型查找
-            print("type")
             self.findbytype()
         elif num == '0':
             sys.exit(0)
@@ -42,18 +40,15 @@ class Lib(object):
             self.continue_use()
 
     def findbytitle(self):              #按照书名查找
-        con = []
         print("input the title")
-        title = '《' + input() + '》'
-        con.append(title)       #对输入加双引号后，转成list
-        result = self.__c.execute("select * from mainstorage where title = ?;", con)   #sqlite查询
+        con = ["%" + input() + "%"]      #对输入加%后，转为模糊搜索格式
+        result = self.__c.execute("select * from mainstorage where title like ?;", con)   #sqlite查询
         x = 0
         for i in result:        #sqlite如果返回为空，则i为0，将显示无此项并提供返回主界面的询问；如果不为空则进入显示结果
             x = 1
-        result = self.__c.execute("select * from mainstorage where title = ?;", con)
+        result = self.__c.execute("select * from mainstorage where title like ?;", con)
         if x == 1:          #查询结果不为空，显示结果
             for inf in result:
-                print(1)
                 print("title :" + inf[0] + '\n',
                       "author :" + inf[1] + '\n',
                       "type :" + inf[2] + '\n',
@@ -66,23 +61,24 @@ class Lib(object):
 
     def findbyauthor(self):
         print("input the author")
-        con = [input()]     #将输入转为list
-        result = self.__c.execute("select title, type from mainstorage where author = ?;", con)
+        con = ["%" + input() + "%"]     #将输入转为list
+        result = self.__c.execute("select title, author, type from mainstorage where author like ?;", con)
         x = 0
         for i in result:        #查询结果为空则为0，显示无此信息并询问是否返回主界面，否则为1，显示结果
             x = 1
-        result = self.__c.execute("select title, type from mainstorage where author = ?;", con)
+        result = self.__c.execute("select title, author, type from mainstorage where author like ?;", con)
         if x == 1:              #查询结果不为空，循环显示所有结果
             for inf in result:
                 print("title :", inf[0], '\n'
-                      "type :", inf[1], '\n')
+                      "author :", inf[1], '\n'
+                      "type :", inf[2], '\n')
             self.continue_use()
         else:           #查询结果为空，询问是否返回主界面
             print("no such author \n back to the main interface")
             self.continue_use()
 
     def findbytype(self):
-        alltype = self.__c.execute("select type from mainstorage group by type having count(type) > 1")
+        alltype = self.__c.execute("select type from mainstorage group by type having count(type) > 1;")
         print("we have types like:")
         i = 0
         for x in alltype:       #显示所有type
@@ -92,24 +88,24 @@ class Lib(object):
                 print(x[0], end="     ")
             i += 1
         print("请输入想要查询的种类")
-        con = [input()]  #将输入转为list
-        result = self.__c.execute("select title, author from mainstorage where type = ?", con)
+        con = ["%" + input() + "%"]  #将输入转为list
+        result = self.__c.execute("select title, author, type from mainstorage where type like ?;", con)
         x = 0
         for i in result:
             x = 1
-        result = self.__c.execute("select title, author from mainstorage where type = ?", con)
+        result = self.__c.execute("select title, author, type from mainstorage where type like ?;", con)
         if x == 1:
             for inf in result:
                 print("title :", inf[0], '\n'
-                      "type :", inf[1], '\n')
+                      "author :", inf[1], '\n'
+                      "type :", inf[2], '\n')
             self.continue_use()
         else:
             print("no such type \n back to the main interface")
             self.continue_use()
 
 
-
-
 test = Lib(cursor)
 test.ui_find()
+
 
